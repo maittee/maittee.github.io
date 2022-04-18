37c5d1893549ea35798a861bfe1965d78ebc5773
maittee <99843013+maittee@users.noreply.github.com>
2022-04-18 19:19:30 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+function encriptar(texto){
+    var textoEncriptado = "";
+    
+    textoEncriptado = texto.replace(/e/g, "enter");
+    textoEncriptado = textoEncriptado.replace(/i/g, "imes");
+    textoEncriptado = textoEncriptado.replace(/a/g, "ai");
+    textoEncriptado = textoEncriptado.replace(/o/g, "ober");
+    textoEncriptado = textoEncriptado.replace(/u/g, "ufat");
+    
+    return textoEncriptado;
+}
+
+function desencriptar(texto){
+    var textoDesencriptado = "";
+    
+    textoDesencriptado = texto.replace(/enter/g, "e");
+    textoDesencriptado = textoDesencriptado.replace(/imes/g, "i");
+    textoDesencriptado = textoDesencriptado.replace(/ai/g, "a");
+    textoDesencriptado = textoDesencriptado.replace(/ober/g, "o");
+    textoDesencriptado = textoDesencriptado.replace(/ufat/g, "u");
+
+    return textoDesencriptado;
+}
+
+function copiarTexto(){
+    var copiarTexto = document.getElementById("text-resultado");
+    
+    copiarTexto.select();
+    copiarTexto.setSelectionRange(0,99999);
+    navigator.clipboard.writeText(copiarTexto.value);
+}
+
+function esTextoValido(texto){
+    var filtro = 'abcdefghijklmnñopqrstuvwxyz ';
+        
+    for (var i = 0; i < texto.length; i++) {
+        if (!esCaracterValido(filtro, texto, i)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function esCaracterValido(texto, letra, i){
+    return texto.indexOf(letra.charAt(i)) != -1;
+}
+
+function esVacio(texto){
+    return texto.length == 0;
+}
+
+function errorMensaje(){
+    var error = document.querySelector("#error");
+    error.textContent = "Error. Ha ingresado un carácter inválido.";
+}
+
+function textoVacioMensaje(){
+    var mensaje = document.querySelector("#error");
+    mensaje.textContent = "No ha ingresado ningún texto.";
+}
+
